7ed975ab14d810ec1e65a618e2e3e5975ec8f594
Trmor <nhjabvqaz@gmail.com>
2023-02-15 12:25:44 +0400

    create page finished

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const DB = require("./DB");
 const http = require('http');
 const express = require("express");
 const { raw } = require("express");
+const { TIME } = require("sequelize");
 const app = express();
 const urlencodedParser = express.urlencoded({extended:false});
 let isLogin = false; //под логин
@@ -49,9 +50,10 @@ app.get("/edit/emergency/:id", function(request, response){
     const cargoclass = DB.CargoClassification.findAll({raw:true});
     const cargostate = DB.CargoState.findAll({raw:true});
     const casualties = DB.Casualties.findAll({raw:true});
+    const infrastructure = DB.Infrastructure.findAll({raw:true});
     const trainNumber = DB.RepairTrain.findAll({raw:true});
     const railway = DB.Railway.findAll({raw:true});
-    Promise.all([emergency, cargoclass, cargostate, casualties, trainNumber, railway])
+    Promise.all([emergency, cargoclass, cargostate, casualties, infrastructure, trainNumber, railway])
     .then(data=>{
     console.log(data);
     response.render("edit/emergency", {
@@ -65,6 +67,23 @@ app.get("/login", function(request, response){
     response.redirect("/emergency")
 });
 
+app.get("/create/emergency", function(request, response){
+
+    const cargoclass = DB.CargoClassification.findAll({raw:true});
+    const cargostate = DB.CargoState.findAll({raw:true});
+    const casualties = DB.Casualties.findAll({raw:true});
+    const infrastructure = DB.Infrastructure.findAll({raw:true});
+    const trainNumber = DB.RepairTrain.findAll({raw:true});
+    const railway = DB.Railway.findAll({raw:true});
+    Promise.all([cargoclass, cargostate, casualties, infrastructure, trainNumber, railway])
+    .then(data=>{
+    console.log(data);
+    response.render("create/emergency", {
+        Emergencies:data
+    });
+    }).catch(err=>console.log(err));
+});
+
 app.post("/edit/emergency", urlencodedParser, function(request, response){
 
     if(!request.body) return response.sendStatus(400);
@@ -74,15 +93,44 @@ app.post("/edit/emergency", urlencodedParser, function(request, response){
     const cargoclass = request.body.cargoclass;
     const cargostate = request.body.cargostate;
     const casualties = request.body.casualties;
+    const infrastructure = request.body.infrastructure;
     const trainNumber = request.body.TrainNumber;
     const geolocation = request.body.geolocation;
     const railway = request.body.railway;
 
     DB.Emergency.update(
         {TrainID:trainId, cargoclassID: cargoclass, cargostateID: cargostate, casualtyID:casualties,
-            repairTrainTrainNumber: trainNumber, Geolocation:geolocation, railwayID: railway},
+            repairtrainTrainNumber: trainNumber, Geolocation:geolocation, railwayID: railway, infrastructureID: infrastructure},
         {where: {ID:id}})
     .then(()=>{
         response.redirect("/emergency")
     }).catch(err=>console.log(err));
 });
+
+app.post("/create/emergency", urlencodedParser, function(request, response){
+
+    if(!request.body) return response.sendStatus(400);
+
+    const trainId = request.body.TrainID;
+    const cargoclass = request.body.cargoclass;
+    const cargostate = request.body.cargostate;
+    const casualties = request.body.casualties;
+    const infrastructure = request.body.infrastructure;
+    const trainNumber = request.body.TrainNumber;
+    const geolocation = request.body.geolocation;
+    const railway = request.body.railway;
+    DB.Emergency.create(
+        {TrainID:trainId, cargoclassID: cargoclass, cargostateID: cargostate, casualtyID:casualties,
+            repairtrainTrainNumber: trainNumber, Geolocation:geolocation, railwayID: railway,
+            Time:Date.now(), infrastructureID:infrastructure})
+    .then(()=>{
+        response.redirect("/emergency")
+    }).catch(err=>console.log(err));
+});
+
+app.post("/delete/emergency/:id", function(request, response){  
+    const id = request.params.id;
+    DB.Emergency.destroy({where: {ID: id} }).then(() => {
+      res.redirect("/emergency");
+    }).catch(err=>console.log(err));
+  });
